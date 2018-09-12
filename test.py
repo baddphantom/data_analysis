d0ba4a0f446057103994e029926a87d392e0a0af
aibraimov <akzhol.ibraimov@gmail.com>
2018-09-12 20:13:32 +0600

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import scipy as sp
+import plotly.plotly as py
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+
+# df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/school_earnings.csv")
+# table = ff.create_table(df)
+# py.iplot(table, filename='jupyter-table1')
+
+
+
+pd.set_option('display.notebook_repr_html', False)
+#print(plt.plot(np.random.randn(1000).cumsum()))
